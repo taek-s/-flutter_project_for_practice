ee768fdc65f0844fc5d10357d972978a7e624bd6
kimst <kimst@dsmentoring.com>
2024-02-05 13:12:56 +0900

    sqliteFfi, databaseFactory 초기화 및 설치
    - sqlite 초기화
    - $ dart run sqflite_common_ffi_web:setup : 해당 명령어 실행하여 setup

--- practice01/lib/main.dart
+++ practice01/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  // sqflite_common_ffi 초기화
+  sqfliteFfiInit();
+
+  // databaseFactory 초기화
+  databaseFactory = databaseFactoryFfiWeb;
+
   runApp(const BaseApp());
 }
