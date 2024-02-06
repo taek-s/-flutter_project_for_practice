6f64dbd32c6573b57e2e65ff67f103082d436da2
kimst <kimst@dsmentoring.com>
2024-02-06 13:32:05 +0900

    [Dog Walking App] Color, Font, svg 설정

---
+++ dog_walking_app/lib/common/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  // BASE Color
+  static const Color _baseColor = Color(0xFF161616);
+
+  // GRADIENTS Color
+  static const Color _gradentsColorRight = Color(0xFFFE8F4B);
+  static const Color _gradentsColorLeft = Color(0xFFFB724C);
+  static const Color _gradentsColorTopRight = Color(0xFFFB7A4B);
+  static const Color _gradentsColorBottomLeft = Color(0xFF9C2CF3);
+
+  // OTHER Color
+  static const Color _blackColor = Color(0xFF383838);
+  static const Color _greyColor01 = Color(0xFF515153);
+  static const Color _greyColor02 = Color(0xFFAEAEB2);
+  static const Color _greyColor03 = Color(0xFFB0B0B0);
+  static const Color _whiteColor = Color(0xFFF0F0F0);
+
+  static const Color _blueColor = Color(0xFF3B5998);
+  static const Color _redColor = Color(0xFFE73A40);
+  static const Color _orangeColor = Color(0xFFFB724C);
+  static const Color _yellowColor = Color(0xFFFFCB55);
+  static const Color _pinkColor01 = Color(0xFFF6D5DC);
+  static const Color _pinkColor02 = Color(0xFFFBE0D8);
+}
