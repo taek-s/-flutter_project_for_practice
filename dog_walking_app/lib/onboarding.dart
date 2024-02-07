f46eeed82d07eaec8bb18742874c0d342f07f612
kimst <kimst@dsmentoring.com>
2024-02-07 14:47:08 +0900

    [Dog Walking App] Onboarding 배경 그라데이션 설정

--- dog_walking_app/lib/onboarding.dart
+++ dog_walking_app/lib/onboarding.dart
@@ -9,18 +9,43 @@ class Onboarding extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData themeData = Theme.of(context);
     return Scaffold(
+      // 배경 이미지 + 텍스트 Stack
       body: Stack(
-        alignment: AlignmentDirectional.center,
-        fit: StackFit.loose,
         children: [
-          Image.asset('/images/onBoarding/onBoarding.png'),
-          Column(
+          // 이미지 그라데이션 용 Stack
+          Stack(
+            alignment: AlignmentDirectional.topCenter,
             children: [
-              Text(
-                'Too tired to walk your dog?',
-                style: themeData.textTheme.titleLarge,
+              // 배경 이미지
+              Image.asset('/images/onBoarding/onBoarding.png'),
+              // 그라데이션 효과용 Container
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      ColorConst.greyColor02.withOpacity(0.1),
+                      ColorConst.baseColor,
+                    ],
+                    stops: [
+                      0.0,
+                      0.75,
+                    ],
+                  ),
+                ),
               ),
             ],
+          ),
+          Container(
+            child: Column(
+              children: [
+                Text(
+                  'Too tired to walk your dog?',
+                  style: themeData.textTheme.titleLarge,
+                ),
+              ],
+            ),
           ),
         ],
       ),
